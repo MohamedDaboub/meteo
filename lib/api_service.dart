5e7d48f096f9cc6d6e362c7ec0680153b18549e9
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2025-01-28 16:14:10 +0100

    Start for the V0.2 change Main.dart add get started all image for meteo

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   final String apiKey =
-      '151b0fc38f6f384bef1787a662378cfc'; // Remplace par ta clé API
+      '151b0fc38f6f384bef1787a662378cfc'; 
   final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   Future<Map<String, dynamic>> getWeather(String city) async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,18 @@
 import 'package:flutter/material.dart';
-import 'api_service.dart';
+import 'package:meteo/ui/get_started.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Météo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WeatherScreen(),
-    );
-  }
-}
-
-class WeatherScreen extends StatefulWidget {
-  @override
-  _WeatherScreenState createState() => _WeatherScreenState();
-}
-
-class _WeatherScreenState extends State<WeatherScreen> {
-  final ApiService _apiService = ApiService();
-  final TextEditingController _cityController = TextEditingController();
-  Map<String, dynamic> _weatherData = {};
-
-  Future<void> _fetchWeather() async {
-    try {
-      final data = await _apiService.getWeather(_cityController.text);
-      setState(() {
-        _weatherData = data;
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Météo App'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _cityController,
-              decoration: InputDecoration(
-                labelText: 'Entrez une ville',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _fetchWeather,
-              child: Text('Voir la météo'),
-            ),
-            SizedBox(height: 20),
-            if (_weatherData.isNotEmpty)
-              Column(
-                children: [
-                  Text(
-                    'Météo à ${_weatherData['name']}',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    '${_weatherData['main']['temp']}°C',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                  Text(
-                    '${_weatherData['weather'][0]['description']}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-          ],
-        ),
-      ),
+      home: GetStarted(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
