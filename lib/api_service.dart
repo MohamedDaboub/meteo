e7856b9e78b4bb024e7d9b1c1b05bd92f1efcfab
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2025-01-28 15:32:14 +0100

    Implement weather fetching functionality and update UI for Météo App V0.1

---
+++ lib/api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String apiKey =
+      '151b0fc38f6f384bef1787a662378cfc'; // Remplace par ta clé API
+  final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<Map<String, dynamic>> getWeather(String city) async {
+    final response = await http
+        .get(Uri.parse('$baseUrl?q=$city&appid=$apiKey&units=metric&lang=fr'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
